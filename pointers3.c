43e1e54d7692f9cafbc2cc37b4eaa68c78c4b1bb
WillRWhite <37294877+WillRWhite@users.noreply.github.com>
2024-03-19 12:55:37 +0000

    pointers3.c - updates to for loops for everything

--- pointers3.c
+++ pointers3.c
@@ -20,12 +20,11 @@ int main(void)
    mychar2 = (char *) malloc(6);
   
     printf("Print array mychar[6] using pointers. Note that p0 points to mychar[0]\n\n");
-    printf("p0+0 = %p points to %i\n", p0, *p0);
-    printf("p0+1 = %p points to %i\n", p0+1, *(p0+1*CHR));
-    printf("p0+2 = %p points to %i\n", p0+2, *(p0+2*CHR));
-    printf("p0+3 = %p points to %i\n", p0+3, *(p0+3*CHR));
-    printf("p0+4 = %p points to %i\n", p0+4, *(p0+4*CHR));
-    printf("p0+5 = %p points to %i\n", p0+5, *(p0+5*CHR));
+
+    for (int i=0; i<6; i++)
+    {
+        printf("p0+%i = %p points to %i\n", i, p0+i, *(p0+i*CHR));
+    }
     printf("\n");
     printf("Note that p0+5 is not correct. Lets fix that by dereferencing it's pointer an allocating 5 to it\n");
     printf("*(p0+5*CHR) = 5\n");
@@ -33,38 +32,33 @@ int main(void)
     *(p0+5*CHR) = 5;
 
     printf("Print again to confirm the fix:\n\n");
-    printf("p0+0 = %p points to %i\n", p0, *p0);
-    printf("p0+1 = %p points to %i\n", p0+1, *(p0+1*CHR));
-    printf("p0+2 = %p points to %i\n", p0+2, *(p0+2*CHR));
-    printf("p0+3 = %p points to %i\n", p0+3, *(p0+3*CHR));
-    printf("p0+4 = %p points to %i\n", p0+4, *(p0+4*CHR));
-    printf("p0+5 = %p points to %i\n", p0+5, *(p0+5*CHR));
+    for (int i=0; i<6; i++)
+    {
+        printf("p0+%i = %p points to %i\n", i, p0+i, *(p0+i*CHR));
+    }
     printf("\n");
-
     printf("Now change the contents using arrary operations and print using pointer arrithematic:\n\n");
 
-  for (int i=0, j=6; i<=5; i++, j++)
+  for (int i=0, j=6; i<6; i++, j++)
     {
         mychar1[i] = j;
     }
 
-    printf("p0+0 = %p points to %i\n", p0, *p0);
-    printf("p0+1 = %p points to %i\n", p0+1, *(p0+1*CHR));
-    printf("p0+2 = %p points to %i\n", p0+2, *(p0+2*CHR));
-    printf("p0+3 = %p points to %i\n", p0+3, *(p0+3*CHR));
-    printf("p0+4 = %p points to %i\n", p0+4, *(p0+4*CHR));
-    printf("p0+5 = %p points to %i\n", p0+5, *(p0+5*CHR));
+   for (int i=0; i<6; i++)
+    {
+        printf("p0+%i = %p points to %i\n", i, p0+i, *(p0+i*CHR));
+    }
     printf("\n");
 
      printf("Now lets look at the malloc allocation, first uninitilised:\n\n");
 
-    printf("mychar2+0 = %p points to %i\n", mychar2, *mychar2);
-    printf("mychar2+1 = %p points to %i\n", mychar2+1, *(mychar2+1*CHR));
-    printf("mychar2+2 = %p points to %i\n", mychar2+2, *(mychar2+2*CHR));
-    printf("mychar2+3 = %p points to %i\n", mychar2+3, *(mychar2+3*CHR));
-    printf("mychar2+4 = %p points to %i\n", mychar2+4, *(mychar2+4*CHR));
-    printf("mychar2+5 = %p points to %i\n", mychar2+5, *(mychar2+5*CHR));
+    for (int i=0; i<6; i++)
+    {
+        printf("mychar2+%i = %p points to %i\n", i, mychar2+1, *(mychar2+1*CHR));
+
+    }
     printf("\n");
+
 
     printf("initilise using pointer arithematic:\n\n");
 
@@ -73,12 +67,11 @@ int main(void)
         *(mychar2+i) = i;
     }
 
-    printf("mychar2+0 = %p points to %i\n", mychar2, *mychar2);
-    printf("mychar2+1 = %p points to %i\n", mychar2+1, *(mychar2+1*CHR));
-    printf("mychar2+2 = %p points to %i\n", mychar2+2, *(mychar2+2*CHR));
-    printf("mychar2+3 = %p points to %i\n", mychar2+3, *(mychar2+3*CHR));
-    printf("mychar2+4 = %p points to %i\n", mychar2+4, *(mychar2+4*CHR));
-    printf("mychar2+5 = %p points to %i\n", mychar2+5, *(mychar2+5*CHR));
+    for (int i=0; i<6; i++)
+    {
+        printf("mychar2+%i = %p points to %i\n", i, mychar2+i, *(mychar2+i*CHR));
+
+    }
     printf("\n");
 
     printf("initilise using array notation. Lets use ASCII from 'A'");
@@ -90,12 +83,11 @@ int main(void)
 
     printf(" and print again using pointers to confirm:\n\n");
 
-    printf("mychar2+0 = %p points to %c\n", mychar2, *mychar2);
-    printf("mychar2+1 = %p points to %c\n", mychar2+1, *(mychar2+1*CHR));
-    printf("mychar2+2 = %p points to %c\n", mychar2+2, *(mychar2+2*CHR));
-    printf("mychar2+3 = %p points to %c\n", mychar2+3, *(mychar2+3*CHR));
-    printf("mychar2+4 = %p points to %c\n", mychar2+4, *(mychar2+4*CHR));
-    printf("mychar2+5 = %p points to %c\n", mychar2+5, *(mychar2+5*CHR));
+  for (int i=0, j=6; i<6; i++, j++)
+    {
+        printf("mychar2+%i = %p points to %c\n", i, mychar2+i, *(mychar2+i*CHR));
+
+    }
     printf("\n");
 
     printf("Lets replace 'E' with 'NUL' and print as a string:\n");
@@ -105,5 +97,20 @@ int main(void)
     //*(mychar2+4) = 0;
 
     printf("%s\n\n", mychar2);
+
+    printf("Now before we free() lets just try printing the interger value of *mychar2. It is:- %i\n", *mychar2);
+
+    free(mychar2);
+
+    printf("After free(), it is:- %i\n\n", *mychar2);
+    printf("And let's just try a few more memory locations:\n\n");
+
+    for (int i=1; i<30; i++)
+    {
+       printf("After free(), mychar2+%i:- %i\n", i, *(mychar2+i)); 
+    }
+    printf("\n");
+
+    return 0;
 
 }
