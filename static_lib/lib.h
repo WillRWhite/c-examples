fc80959aea33bec848f50c6d13997a5fa60475de
WillRWhite <37294877+WillRWhite@users.noreply.github.com>
2024-03-22 16:07:07 +0000

    Minor updates

--- static_lib/lib.h
+++ static_lib/lib.h
@@ -1,2 +1,5 @@
-float add(float a, float b);
-float mult(float a, float b);
+// extern keyword is not actually required becasue it is implied for all
+// functions but maybe useful to remind the user that in the case of a 
+// library the function will be founs outside of the program
+extern float add(float a, float b);
+extern float mult(float a, float b);
--- static_lib/main.c
+++ static_lib/main.c
@@ -4,11 +4,11 @@
 
 int main(void)
 {
-    float a = 5;
-    float b = 6;
+    float a = 5.4;
+    float b = 6.7;
 
     float x = add(a,b);
     float y = mult(a,b);
 
-    printf("\n%f %f %f %f\n\n",a,b,x,y);
+    printf("\n%.2f %.2f %.2f %.2f\n\n",a,b,x,y);
 }
