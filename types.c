13a1580ebef165d8d6a2f990e1e8f06dd4da1088
WillRWhite <37294877+WillRWhite@users.noreply.github.com>
2024-03-26 10:51:47 +0000

    Added types.c

---
+++ types.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    // Print i as an integer
+    for(int i=65; i<=122; i++)
+    {
+        printf("%i ",i);
+    }
+     printf("\n");
+     // Print i as a char
+    for(int i=65; i<=122; i++)
+    {
+        printf("%c ",i);
+    }
+    printf("\n");
+}
