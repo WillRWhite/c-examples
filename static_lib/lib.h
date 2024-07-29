fbcbd607fb932bbf0b882e31f8456fe6084f946c
WillRWhite <37294877+WillRWhite@users.noreply.github.com>
2024-07-29 16:56:12 +0100

    Update lib.h

--- static_lib/lib.h
+++ static_lib/lib.h
@@ -1,5 +1,5 @@
 // extern keyword is not actually required becasue it is implied for all
 // functions but maybe useful to remind the user that in the case of a 
-// library the function will be founs outside of the program
+// library the function will be found outside of the program
 extern float add(float a, float b);
 extern float mult(float a, float b);
